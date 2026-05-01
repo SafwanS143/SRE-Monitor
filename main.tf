3c1b19a9524aa4790a2a9ed82b5574e99437c139
Safwan Shiblee <shibleesafwan@gmail.com>
2026-05-01 19:30:28 -0400

    Added user ownership of project file

--- main.tf
+++ main.tf
@@ -24,10 +24,11 @@ resource "aws_instance" "monitor_target" {
     systemctl start docker
     systemctl enable docker
     usermod -aG docker ec2-user
+    mkdir -p /home/ec2-user/sre-project
+    chown -R ec2-user:ec2-user /home/ec2-user/sre-project
     mkdir -p /usr/local/lib/docker/cli-plugins
     curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
     chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-    mkdir -p /home/ec2-user/sre-project
   EOF
 
   tags = {
