f5fe5e0f5ed077d751ac8e60c2d25c54fed07248
Safwan Shiblee <shibleesafwan@gmail.com>
2026-05-03 19:34:27 -0400

    Specified Buildx and Compose docker versions in userdata

--- main.tf
+++ main.tf
@@ -27,9 +27,9 @@ resource "aws_instance" "monitor_target" {
     mkdir -p /home/ec2-user/sre-project
     chown -R ec2-user:ec2-user /home/ec2-user/sre-project
     mkdir -p /usr/local/lib/docker/cli-plugins
-    curl -SL https://github.com/docker/buildx/releases/latest/download/buildx-linux-amd64 -o /usr/local/lib/docker/cli-plugins/docker-buildx
+    curl -SL https://github.com/docker/buildx/releases/download/v0.13.1/buildx-v0.13.1.linux-amd64 -o /usr/local/lib/docker/cli-plugins/docker-buildx
     chmod +x /usr/local/lib/docker/cli-plugins/docker-buildx
-    curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+    curl -SL https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
     chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
   EOF
 
